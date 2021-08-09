5bb51851b7a65e88db8d2f04e44578cbe17880cf
tangjizhou <tangjizhouchn@foxmail.com>
2021-08-09 16:07:55 +0800

    卸载忽略错误

--- roles/install-etcd-backup-cron/templates/backup_etcd.sh
+++ roles/install-etcd-backup-cron/templates/backup_etcd.sh
@@ -1,8 +1,12 @@
+# back up
+backup_dir=/data01/etcd/backup
 ETCDCTL_API=3 etcdctl --endpoints=https://127.0.0.1:2379 \
 --cacert=/etc/kubernetes/pki/etcd/ca.crt \
 --cert=/etc/kubernetes/pki/etcd/server.crt \
 --key=/etc/kubernetes/pki/etcd/server.key \
-snapshot save /backup/etcd-snapshot-$(date +%Y%m%d).db
+snapshot save $backup_dir/etcd-snapshot-$(date +%Y%m%d).db
 
+# remove old
+find $backup_dir -mtime +7
 
 
