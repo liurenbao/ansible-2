d519b28cce44b82e2d7d016e226381d85634c955
tangjizhou <tangjizhouchn@foxmail.com>
2021-10-22 12:09:54 +0800

    nginx filebeat直接安装

--- roles/k8s-install-elk/templates/auto-create-kibana-pattern.sh
+++ roles/k8s-install-elk/templates/auto-create-kibana-pattern.sh
@@ -4,6 +4,7 @@ indices=$(curl http://192.168.160.36:9200/_cat/indices/log-*-$year_month-* | cut
 for index in $indices[*]; do
     index_prefix=${index%${year_month}*}
     pattern=$index_prefix$year_month-*
+
     curl -XPOST -d@- --url http://192.168.160.36:30001/api/index_patterns/index_pattern -H "Content-Type: application/json" -H "kbn-version: 7.13.2" << EOF
     {
       "index_pattern": {
