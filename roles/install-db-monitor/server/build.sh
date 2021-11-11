14890c6a774563acbca6b01238f98b867fb03f9f
tangjizhou <tangjizhouchn@foxmail.com>
2021-11-11 18:18:28 +0800

    高斯监控版本升级，配置修改

--- roles/install-db-monitor/server/build.sh
+++ roles/install-db-monitor/server/build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-docker build . -t 192.168.160.47:8888/public/uoamp:20211028-dev
-docker push 192.168.160.47:8888/public/uoamp:20211028-dev
-docker rmi 192.168.160.47:8888/public/uoamp:20211028-dev
+docker build . -t 192.168.160.47:8888/public/monitor-server:20211028-dev
+docker push 192.168.160.47:8888/public/monitor-server:20211028-dev
+docker rmi 192.168.160.47:8888/public/monitor-server:20211028-dev
 
 kubectl apply -f deployment.yml -n monitoring
