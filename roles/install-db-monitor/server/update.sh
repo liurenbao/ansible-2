6fb01670814d454a64d951b2921786993931a164
tangjizhou <tangjizhouchn@foxmail.com>
2021-12-08 23:34:06 +0800

    数据库监控升级

--- roles/install-db-monitor/server/update.sh
+++ roles/install-db-monitor/server/update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker build . -t 10.12.107.150:80/public/monitor-server:20211125-dev
-docker push 10.12.107.150:80/public/monitor-server:20211125-dev
-docker rmi 10.12.107.150:80/public/monitor-server:20211125-dev
+docker build . -t 10.12.107.150:80/public/monitor-server:20211207-dev
+docker push 10.12.107.150:80/public/monitor-server:20211207-dev
+docker rmi 10.12.107.150:80/public/monitor-server:20211207-dev
 
-kubectl set image deployment/monitor-server monitor-server=10.12.107.150:80/public/monitor-server:20211125-dev -n gaussdb-monitoring
+kubectl set image deployment/monitor-server monitor-server=10.12.107.150:80/public/monitor-server:20211207-dev -n gaussdb-monitoring
 kubectl rollout restart deployment/monitor-server -n gaussdb-monitoring
 kubectl rollout status deployment/monitor-server -n gaussdb-monitoring
