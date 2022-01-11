5844d55dabbbfaedae668218fb5a29ebc6b2129b
root <test@123.com>
2022-01-11 14:20:34 +0800

    vip网络校验

--- roles/install-keepalived/files/apiserver_healthcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ -z "`ss -nltp | grep kube-apiserver | grep 6443 | grep LISTEN`" ]; then
-  echo "exit 1"
-  exit 1
-fi
--- roles/install-keepalived/files/nginx_healthcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ -z "`docker ps | grep 'nginx:' | grep 'Up' `" ]; then
-  echo "exit 1"
-  exit 1
-fi
