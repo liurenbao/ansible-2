e7f17039bd1092179c33d14126bc93091d258f0f
tangjizhou <tangjizhouchn@foxmail.com>
2022-02-21 15:27:56 +0800

    高斯监控更新

--- roles/install-gaussdb-monitor-agent/server/build.sh
+++ roles/install-gaussdb-monitor-agent/server/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build . -t 10.12.107.150:80/public/monitor-server:20220113-dev
-docker push 10.12.107.150:80/public/monitor-server:20220113-dev
-docker rmi 10.12.107.150:80/public/monitor-server:20220113-dev
+docker build . -t 10.12.107.150:80/public/monitor-server:20220216-dev
+docker push 10.12.107.150:80/public/monitor-server:20220216-dev
+docker rmi 10.12.107.150:80/public/monitor-server:20220216-dev
 
 kubectl apply -f deployment.yml -n gaussdb-monitoring
